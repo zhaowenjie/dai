6287b83fb6e567f5c824cf81c162ea115921b276
赵文杰 <zhaowenjie@surongtec.com>
2019-12-17 15:29:54 +0800

    u

---
+++ precache-manifest.b24d7ba4b87dc201ecadd7ea5c45cfc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4efd83114355505b49c88067ce3cfb",
+    "url": "/dai/index.html"
+  },
+  {
+    "revision": "0cee1473642ad3d7e40a",
+    "url": "/dai/static/css/main.eae635f5.chunk.css"
+  },
+  {
+    "revision": "adf607ed83775399adac",
+    "url": "/dai/static/js/2.01cb1624.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dai/static/js/2.01cb1624.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0cee1473642ad3d7e40a",
+    "url": "/dai/static/js/main.a2932537.chunk.js"
+  },
+  {
+    "revision": "2b097b0992edccbd0f84",
+    "url": "/dai/static/js/runtime-main.0d9cf24a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dai/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.176bccfafa054f516d6d196bee20a5f7.js"
+  "/dai/precache-manifest.b24d7ba4b87dc201ecadd7ea5c45cfc0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dai/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
